5b786fb7bdb593aea9d81029ba940c9c2dba38b3
Greg Anderson <greg.anderson@greenknowe.org>
2025-02-02 04:15:32 -0800

    Switch terms from attacker / defender to active player / opponent

--- combat-odds.py
+++ combat-odds.py
@@ -51,30 +51,30 @@ class ContestDice:
         return sides - (self.t - 1)
 
 class OutcomeProbabilities:
-    attacker_odds = 0
-    defender_odds = 0
+    active_player_odds = 0
+    opponent_odds = 0
     odds_of_tie = 0
 
     def tie(self, odds):
         self.odds_of_tie += odds
         return self
 
-    def attacker_wins(self, odds):
-        self.attacker_odds += odds
+    def active_player_wins(self, odds):
+        self.active_player_odds += odds
         return self
 
-    def defender_wins(self, odds):
-        self.defender_odds += odds
+    def opponent_wins(self, odds):
+        self.opponent_odds += odds
         return self
 
     def combine(self, cell_probability, sub_odds):
         self.tie(cell_probability * sub_odds.odds_of_tie)
-        self.attacker_wins(cell_probability * sub_odds.attacker_odds)
-        self.defender_wins(cell_probability * sub_odds.defender_odds)
+        self.active_player_wins(cell_probability * sub_odds.active_player_odds)
+        self.opponent_wins(cell_probability * sub_odds.opponent_odds)
 
     def print(self):
-        print("Attacker wins: " + str(self.attacker_odds))
-        print("Defender wins: " + str(self.defender_odds))
+        print("Active Player wins: " + str(self.active_player_odds))
+        print("Opponent wins: " + str(self.opponent_odds))
         print("Odds of tie:   " + str(self.odds_of_tie))
 
 class Accumulator:
@@ -113,12 +113,12 @@ class HTMLOutput(Accumulator):
         for row in self.rows:
             rowHeader = str(row[0])
             rowData = list(map(lambda p: self.cell_contents(p), row[1:]))
-            print("<tr><th class=defender-row-header id='d-" + rowHeader + "'>" + rowHeader + "</th>\t" + "\t".join(rowData) + "\t</tr>")
+            print("<tr><th class=opponent-row-header id='d-" + rowHeader + "'>" + rowHeader + "</th>\t" + "\t".join(rowData) + "\t</tr>")
         print("</table>")
         page.print_footer()
 
     def column_header(self, h):
-        return f"<th class=attacker-col-header id='a-{h}'>{h}</th>"
+        return f"<th class=active-player-col-header id='a-{h}'>{h}</th>"
 
     def cell_contents(self, p):
         style = self.style_lookup(p)
@@ -131,13 +131,13 @@ class HTMLOutput(Accumulator):
         return ''
 
 class TwoCombatantsPage:
-    attacker_targeting = 1
-    defender_targeting = 1
+    active_player_targeting = 1
+    opponent_targeting = 1
     local = True
 
-    def __init__(self, attacker_targeting, defender_targeting, local):
-        self.attacker_targeting = attacker_targeting
-        self.defender_targeting = defender_targeting
+    def __init__(self, active_player_targeting, opponent_targeting, local):
+        self.active_player_targeting = active_player_targeting
+        self.opponent_targeting = opponent_targeting
         self.local = local
 
     def print_header(self):
@@ -145,13 +145,13 @@ class TwoCombatantsPage:
         print(f"<html><head><link rel='stylesheet' href='{css_path}/page.css'></head><body>")
         print("<a href='https://github.com/g1a/andromedas-edge-odds' class='github-fork-ribbon'data-ribbon='Fork me on GitHub' title='Fork me on GitHub'>Fork me on GitHub</a>")
         print("<div class='contents'>")
-        print(f"<div class='horizontal-menu'><h3>Attacker</h3>")
-        print("<ul id='attacker-menu'>")
-        self.print_menu_contents(self.attacker_targeting, f"VAR-targeting-v-{self.defender_targeting}-targeting")
+        print(f"<div class='horizontal-menu'><h3>Active Player</h3>")
+        print("<ul id='active_player-menu'>")
+        self.print_menu_contents(self.active_player_targeting, f"VAR-targeting-v-{self.opponent_targeting}-targeting")
         print("</ul></div>")
-        print(f"<div class='vertical-menu'><h3>Defender</h3>")
-        print("<ul id='defender-menu'>")
-        self.print_menu_contents(self.defender_targeting, f"{self.attacker_targeting}-targeting-v-VAR-targeting")
+        print(f"<div class='vertical-menu'><h3>Opponent</h3>")
+        print("<ul id='opponent-menu'>")
+        self.print_menu_contents(self.opponent_targeting, f"{self.active_player_targeting}-targeting-v-VAR-targeting")
         print("</ul></div>")
 
     def print_footer(self):
@@ -168,64 +168,64 @@ class TwoCombatantsPage:
             else:
                 print("<li class=unselected><a href='" + link + "'>" + str(menu) + "</a></li>")
 
-def calculate_outcome_probabilities(attacker: ContestDice, defender: ContestDice, sides: int):
+def calculate_outcome_probabilities(active_player: ContestDice, opponent: ContestDice, sides: int):
     outcome = OutcomeProbabilities()
 
-    if sides <= 0 or (attacker.number() == 0 and defender.number() ==0):
+    if sides <= 0 or (active_player.number() == 0 and opponent.number() ==0):
         return outcome.tie(1)
-    if attacker.number() <= 0:
-        return outcome.defender_wins(1)
-    if defender.number() <= 0:
-        return outcome.attacker_wins(1)
+    if active_player.number() <= 0:
+        return outcome.opponent_wins(1)
+    if opponent.number() <= 0:
+        return outcome.active_player_wins(1)
 
-    for attacker_highest_count in range(7):
-        for defender_highest_count in range(7):
-            attacker_probability = binomial_probability(attacker.number(), attacker_highest_count, attacker.effective_sides(sides))
-            defender_probability = binomial_probability(defender.number(), defender_highest_count, defender.effective_sides(sides))
-            cell_probability = attacker_probability * defender_probability
+    for active_player_highest_count in range(7):
+        for opponent_highest_count in range(7):
+            active_player_probability = binomial_probability(active_player.number(), active_player_highest_count, active_player.effective_sides(sides))
+            opponent_probability = binomial_probability(opponent.number(), opponent_highest_count, opponent.effective_sides(sides))
+            cell_probability = active_player_probability * opponent_probability
             if cell_probability > 0:
-                if attacker_highest_count > defender_highest_count:
-                    outcome.attacker_wins(cell_probability)
-                elif attacker_highest_count < defender_highest_count:
-                    outcome.defender_wins(cell_probability)
+                if active_player_highest_count > opponent_highest_count:
+                    outcome.active_player_wins(cell_probability)
+                elif active_player_highest_count < opponent_highest_count:
+                    outcome.opponent_wins(cell_probability)
                 else:
-                    sub_attacker = ContestDice(attacker.number() - attacker_highest_count, attacker.targeting())
-                    sub_defender = ContestDice(defender.number() - defender_highest_count, defender.targeting())
-                    sub_odds = calculate_outcome_probabilities(sub_attacker, sub_defender, sides - 1)
+                    sub_active_player = ContestDice(active_player.number() - active_player_highest_count, active_player.targeting())
+                    sub_opponent = ContestDice(opponent.number() - opponent_highest_count, opponent.targeting())
+                    sub_odds = calculate_outcome_probabilities(sub_active_player, sub_opponent, sides - 1)
                     outcome.combine(cell_probability, sub_odds)
     
     return outcome
 
-def contest_table_for_targeting(attacker_targeting, defender_targeting, accumulator):
+def contest_table_for_targeting(active_player_targeting, opponent_targeting, accumulator):
     accumulator.setHeader("D  \\ A", [str(i) for i in range(1,7)])
-    for defender_dice in range(1, 7):
+    for opponent_dice in range(1, 7):
         row = []
-        for attacker_dice in range(1, 7):
-            attacker_contest_dice = ContestDice(attacker_dice, attacker_targeting)
-            defender_contest_dice = ContestDice(defender_dice, defender_targeting)
-            outcome = calculate_outcome_probabilities(attacker_contest_dice, defender_contest_dice, 6)
-            row.append(outcome.attacker_odds)
-        accumulator.addRow(defender_dice, row)
+        for active_player_dice in range(1, 7):
+            active_player_contest_dice = ContestDice(active_player_dice, active_player_targeting)
+            opponent_contest_dice = ContestDice(opponent_dice, opponent_targeting)
+            outcome = calculate_outcome_probabilities(active_player_contest_dice, opponent_contest_dice, 6)
+            row.append(outcome.active_player_odds)
+        accumulator.addRow(opponent_dice, row)
 
 def probability_table(n, s):
     # Create a 7x7 table for possible outcomes (0 to 6 sixes for each player)
     print(f"Probability Table for {n} dice with {s}-sided dice:")
     
-    # Print header row (attacker's number of sixes)
-    header = ["Defender / Attacker"] + [str(i) for i in range(7)]
+    # Print header row (active_player's number of sixes)
+    header = ["Opponent / Active"] + [str(i) for i in range(7)]
     print("\t".join(header))
     
-    # Calculate and print each row (defender's number of sixes)
-    for defender_sixes in range(7):
-        row = [str(defender_sixes)]
-        for attacker_sixes in range(7):
-            # Get the probability of the defender rolling exactly defender_sixes
-            # and the attacker rolling exactly attacker_sixes
-            p_defender = binomial_probability(n, defender_sixes, s)
-            p_attacker = binomial_probability(n, attacker_sixes, s)
+    # Calculate and print each row (opponent's number of sixes)
+    for opponent_sixes in range(7):
+        row = [str(opponent_sixes)]
+        for active_player_sixes in range(7):
+            # Get the probability of the opponent rolling exactly opponent_sixes
+            # and the active_player rolling exactly active_player_sixes
+            p_opponent = binomial_probability(n, opponent_sixes, s)
+            p_active_player = binomial_probability(n, active_player_sixes, s)
             
             # The joint probability is the product of the two individual probabilities
-            joint_probability = p_defender * p_attacker
+            joint_probability = p_opponent * p_active_player
             
             # Convert to percentage and append to the row
             row.append(f"{joint_probability * 100:.2f}%")
@@ -236,18 +236,18 @@ def probability_table(n, s):
 
 # TODO: Use a cli parser
 if (len(sys.argv) < 3):
-    print("Usage: python combat-odds.py attacker_targeting defender_targeting")
+    print("Usage: python combat-odds.py active_player_targeting opponent_targeting")
     sys.exit(1)
 
-attacker_targeting = int(sys.argv[1])
-defender_targeting = int(sys.argv[2])
+active_player_targeting = int(sys.argv[1])
+opponent_targeting = int(sys.argv[2])
 local = False
 
 accumulator = PlaintextOutput()
 if ((len(sys.argv) >= 4) and (sys.argv[3] == '--html')):
-    page = TwoCombatantsPage(attacker_targeting, defender_targeting, local)
+    page = TwoCombatantsPage(active_player_targeting, opponent_targeting, local)
     accumulator = HTMLOutput(page, {0: 'unlikely', 0.2: 'low', 0.45: 'even', 0.55: 'high', 0.8: 'likely'})
 
-contest_table_for_targeting(attacker_targeting, defender_targeting, accumulator)
+contest_table_for_targeting(active_player_targeting, opponent_targeting, accumulator)
 
 accumulator.print()
